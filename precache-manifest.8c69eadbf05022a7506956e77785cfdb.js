9c9d39c97125944572e97b2f6e9ad331634cb847
Kaitlyn Nguyen <kn104668@harriscomputer.com>
2022-10-08 00:34:46 -0700

    Updates

---
+++ precache-manifest.8c69eadbf05022a7506956e77785cfdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "911d969e9170fb114440",
+    "url": "/airforce_web/static/css/main.45d88d31.chunk.css"
+  },
+  {
+    "revision": "911d969e9170fb114440",
+    "url": "/airforce_web/static/js/main.084c6c11.chunk.js"
+  },
+  {
+    "revision": "96ba7b6919918dd1b909",
+    "url": "/airforce_web/static/js/runtime~main.c2ab32ea.js"
+  },
+  {
+    "revision": "a8c831acf91ac01ba474",
+    "url": "/airforce_web/static/js/2.77ad1778.chunk.js"
+  },
+  {
+    "revision": "32521fdfddeab7039e98ba38ced08cc7",
+    "url": "/airforce_web/static/media/brand.32521fdf.png"
+  },
+  {
+    "revision": "100bf38cd030c9a8628f1a32d4a7ee89",
+    "url": "/airforce_web/index.html"
+  }
+];
