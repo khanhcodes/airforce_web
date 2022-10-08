362fb6eaa13fce386b826318eb95adc165a07722
Kaitlyn Nguyen <kn104668@harriscomputer.com>
2022-10-08 00:50:01 -0700

    fix toggle-bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,23 +7,23 @@ const Navbar = () => {
   return (
     <nav className='body'>
         <div className='navbar-container'>
-            <a href='/'>
+            <a href='#'>
                 <img className='logo' src={Logo} alt="Logo">
                 </img>
             </a>
-            <a href='/' className='toggle-button'>
+            <a href='#' className='toggle-button'>
                 <span className='bar'></span>
                 <span className='bar'></span>
                 <span className='bar'></span>
             </a>
             <div className='navbar-links'>
                 <ul>
-                    <li><a href='/'>75TH ANNIVERSARY</a></li>
-                    <li><a href='/'>NEWSROOM</a></li>
-                    <li><a href='/'>BIOGRAPHIES</a></li>
-                    <li><a href='/'>ABOUT US</a></li>
-                    <li><a href='/'>AF SENIOR LEADERS</a></li>
-                    <li><a href='/'>COVID-19</a></li>
+                    <li><a href='#'>75TH ANNIVERSARY</a></li>
+                    <li><a href='#'>NEWSROOM</a></li>
+                    <li><a href='#'>BIOGRAPHIES</a></li>
+                    <li><a href='#'>ABOUT US</a></li>
+                    <li><a href='#'>AF SENIOR LEADERS</a></li>
+                    <li><a href='#'>COVID-19</a></li>
                 </ul>
             </div>
         </div>
